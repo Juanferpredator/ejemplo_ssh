c99261c5bb13a9c8d80269f03d982ff99da6f09b
JuanFerR <juanfersoftdeveloper@gmail.com>
2026-05-07 16:56:20 -0500

    new dog class

--- practice.py
+++ practice.py
@@ -1,2 +1,19 @@
 #first line of code in python
 print("Hello World, I am Juan Fernando Rojas from palmira.")
+class perro:
+    def __init__(self, nombre, edad, raza):
+        self.nombre = nombre
+        self.edad = edad
+        self.raza = raza
+    def Ladrar(self):
+        print("guauuu")
+        
+    def comer(self):
+        print("ñam ñam")
+        
+perro1 = perro("boky", 4, "labrador")
+perro2 = perro("rocky", 2, "pastor aleman")
+print(perro1.nombre)
+print(perro2.nombre)
+perro1.Ladrar()
+perro2.comer()
